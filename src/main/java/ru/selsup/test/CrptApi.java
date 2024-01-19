d5d1f047847e3eeb9e259bab144108a9f56aef41
t3hVega <ivan.laganchuk@gmail.com>
2024-01-19 21:41:59 +0300

    Доработка

--- src/main/java/ru/selsup/test/CrptApi.java
+++ src/main/java/ru/selsup/test/CrptApi.java
@@ -1,99 +1,101 @@
 package ru.selsup.test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class CrptApi {
+    private static String targetUri = "https://ismp.crpt.ru/api/v3/lk/documents/create";
+    private static String selfUri = "http://localhost:8080";
     Logger logger = Logger.getLogger(CrptApi.class.getName());
-    private ArrayList<Document> documents = new ArrayList<>();
-    private LinkedList<HttpRequest> queue = new LinkedList<>();
-    private boolean isProcessingActive;
     private boolean isLimitReached = false;
+    private boolean isProcessingActive = false;
     private int numOfRequests;
     private final HttpClient httpClient;
     private final TimeUnit timeUnit;
     private final long duration;
     private final int requestLimit;
-    ObjectMapper objectMapper = new ObjectMapper();
+    private final Semaphore requestSemaphore;
     private Document document = new Document();
+    ObjectMapper objectMapper = new ObjectMapper();
 
     public CrptApi(HttpClient httpClient, TimeUnit timeUnit, long duration, int requestLimit) {
+        if (requestLimit <= 0) {
+            throw new IllegalArgumentException("Указано недопустимое число максимальных запросов.");
+        }
         this.httpClient = httpClient;
         this.timeUnit = timeUnit;
         this.duration = duration;
         this.requestLimit = requestLimit;
+        this.requestSemaphore = new Semaphore(requestLimit, true);
     }
 
-    public void processRequest(HttpRequest httpRequest) throws IOException, InterruptedException {
-        if (!isProcessingActive) {
-            initializeProcessing();
-        }
-        if(isProcessingActive && numOfRequests < requestLimit) {
-            placeDocumentFromRequest(httpRequest);
-        }
-        if (numOfRequests == requestLimit) isLimitReached = true;
-        numOfRequests++;
-        if (isLimitReached) {
-            queue.add(httpRequest);
-        }
-        logger.info("Кол-во документов: " + documents.size() + "\n" +
-                "Кол-во запросов: " + numOfRequests + "\n" +
-                "Запросов в очереди: " + queue.size());
+    private String convertDocumentToJson(Document document) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(document);
     }
 
-    private void placeDocumentFromRequest(HttpRequest httpRequest) throws IOException, InterruptedException {
+    private HttpRequest generateHttpRequest(String jsonDocument, String signature) {
+        return HttpRequest.newBuilder()
+                .uri(URI.create(selfUri))
+                .header("Content-Type", "application/json")
+                .header("Signature", signature)
+                .POST(HttpRequest.BodyPublishers.ofString(jsonDocument))
+                .build();
+    }
+
+    private void sendRequest (HttpRequest httpRequest) throws IOException, InterruptedException {
         HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-        String documentContents = response.body();
-        Document document = objectMapper.readValue(documentContents, Document.class);
-        documents.add(document);
+        if (response.statusCode() == 200) {
+            logger.info("Запрос успешно обработан");
+        } else {
+            logger.warning("Возникла ошибка при отправлении запроса, код ошибки: " + response.statusCode());
+        }
     }
 
-    private void initializeProcessing() {
-        logger.info("Процесс начался");
+    public void createDocument(Document document, String signature) throws IOException, InterruptedException {
+        String jsonDocument = convertDocumentToJson(document);
+        HttpRequest httpRequest = generateHttpRequest(jsonDocument, signature);
+        if (!isProcessingActive) {
+            isProcessingActive = true;
+            signaller();
+        }
+        numOfRequests++;
+        if (numOfRequests == requestLimit) {
+            isLimitReached = true;
+        }
+        if (requestSemaphore.tryAcquire(duration, timeUnit)) {
+            sendRequest(httpRequest);
+            logger.info(String.valueOf(requestSemaphore.availablePermits()));
+        }
+
+    }
+
+    private void signaller() {
         long start = System.currentTimeMillis();
         long end = start + timeUnit.toMillis(duration);
-        numOfRequests = 0;
-        isProcessingActive = true;
-        isLimitReached = false;
         Thread thread = new Thread(() -> {
             while (true) {
-                try {
-                    Thread.sleep(500);
-                    if (System.currentTimeMillis() > end) {
-                        isProcessingActive = false;
-                        Thread.currentThread().interrupt();
-                    }
-                } catch (InterruptedException e) {
-                    if (System.currentTimeMillis() > end) {
-                        isProcessingActive = false;
-                        logger.info("Процесс завершился");
-                        if (!queue.isEmpty()) {
-                            try {
-                                int queueSize = queue.size();
-                                for (int i = 0; i < queueSize; i++) {
-                                    HttpRequest firstInQueue = queue.getFirst();
-                                    queue.removeFirst();
-                                    processRequest(firstInQueue);
-                                }
-                            } catch (IOException ex) {
-                                throw new RuntimeException(ex);
-                            } catch (InterruptedException ex) {
-                                throw new RuntimeException(ex);
-                            }
-                        }
-                        Thread.currentThread().interrupt();
-                        throw new RuntimeException(e);
-                    }
+                if (System.currentTimeMillis() > end) {
+                    requestSemaphore.release(requestLimit - requestSemaphore.availablePermits());
+                    isProcessingActive = false;
+                    numOfRequests = 0;
+                    isLimitReached = false;
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException();
                 }
             }
+
+
         });
         thread.start();
     }
@@ -102,104 +104,7 @@ public class CrptApi {
         return document;
     }
 
-    static class Document {
-
-        static class Description{
-            private String participantInn = "string";
-
-            public String getParticipantInn() {
-                return participantInn;
-            }
-
-            public void setParticipantInn(String participantInn) {
-                this.participantInn = participantInn;
-            }
-        }
-
-         static class Product{
-            private String certificateDocument = "string";
-            private String certificateDocumentDate = "2020-01-23";
-            private String certificateDocumentNumber = "string";
-            private String ownerInn = "string";
-            private String producerInn = "string";
-            private String productionDate = "2020-01-23";
-            private String tnvedCode = "string";
-            private String uitCode = "string";
-            private String uituCode = "string";
-
-            public String getCertificateDocument() {
-                return certificateDocument;
-            }
-
-            public void setCertificateDocument(String certificateDocument) {
-                this.certificateDocument = certificateDocument;
-            }
-
-            public String getCertificateDocumentDate() {
-                return certificateDocumentDate;
-            }
-
-            public void setCertificateDocumentDate(String certificateDocumentDate) {
-                this.certificateDocumentDate = certificateDocumentDate;
-            }
-
-            public String getCertificateDocumentNumber() {
-                return certificateDocumentNumber;
-            }
-
-            public void setCertificateDocumentNumber(String certificateDocumentNumber) {
-                this.certificateDocumentNumber = certificateDocumentNumber;
-            }
-
-            public String getOwnerInn() {
-                return ownerInn;
-            }
-
-            public void setOwnerInn(String ownerInn) {
-                this.ownerInn = ownerInn;
-            }
-
-            public String getProducerInn() {
-                return producerInn;
-            }
-
-            public void setProducerInn(String producerInn) {
-                this.producerInn = producerInn;
-            }
-
-            public String getProductionDate() {
-                return productionDate;
-            }
-
-            public void setProductionDate(String productionDate) {
-                this.productionDate = productionDate;
-            }
-
-            public String getTnvedCode() {
-                return tnvedCode;
-            }
-
-            public void setTnvedCode(String tnvedCode) {
-                this.tnvedCode = tnvedCode;
-            }
-
-            public String getUitCode() {
-                return uitCode;
-            }
-
-            public void setUitCode(String uitCode) {
-                this.uitCode = uitCode;
-            }
-
-            public String getUituCode() {
-                return uituCode;
-            }
-
-            public void setUituCode(String uituCode) {
-                this.uituCode = uituCode;
-            }
-        }
-
+    public static class Document {
         private Description description = new Description();
         private String docId = "string";
         private String docStatus = "string";
@@ -318,6 +223,102 @@ public class CrptApi {
 
         public void setRegNumber(String regNumber) {
             this.regNumber = regNumber;
+        }
+    }
+
+    public static class Description{
+        private String participantInn = "string";
+
+        public String getParticipantInn() {
+            return participantInn;
+        }
+
+        public void setParticipantInn(String participantInn) {
+            this.participantInn = participantInn;
+        }
+    }
+
+    public static class Product{
+        private String certificateDocument = "string";
+        private String certificateDocumentDate = "2020-01-23";
+        private String certificateDocumentNumber = "string";
+        private String ownerInn = "string";
+        private String producerInn = "string";
+        private String productionDate = "2020-01-23";
+        private String tnvedCode = "string";
+        private String uitCode = "string";
+        private String uituCode = "string";
+
+        public String getCertificateDocument() {
+            return certificateDocument;
+        }
+
+        public void setCertificateDocument(String certificateDocument) {
+            this.certificateDocument = certificateDocument;
+        }
+
+        public String getCertificateDocumentDate() {
+            return certificateDocumentDate;
+        }
+
+        public void setCertificateDocumentDate(String certificateDocumentDate) {
+            this.certificateDocumentDate = certificateDocumentDate;
+        }
+
+        public String getCertificateDocumentNumber() {
+            return certificateDocumentNumber;
+        }
+
+        public void setCertificateDocumentNumber(String certificateDocumentNumber) {
+            this.certificateDocumentNumber = certificateDocumentNumber;
+        }
+
+        public String getOwnerInn() {
+            return ownerInn;
+        }
+
+        public void setOwnerInn(String ownerInn) {
+            this.ownerInn = ownerInn;
+        }
+
+        public String getProducerInn() {
+            return producerInn;
+        }
+
+        public void setProducerInn(String producerInn) {
+            this.producerInn = producerInn;
+        }
+
+        public String getProductionDate() {
+            return productionDate;
+        }
+
+        public void setProductionDate(String productionDate) {
+            this.productionDate = productionDate;
+        }
+
+        public String getTnvedCode() {
+            return tnvedCode;
+        }
+
+        public void setTnvedCode(String tnvedCode) {
+            this.tnvedCode = tnvedCode;
+        }
+
+        public String getUitCode() {
+            return uitCode;
+        }
+
+        public void setUitCode(String uitCode) {
+            this.uitCode = uitCode;
+        }
+
+        public String getUituCode() {
+            return uituCode;
+        }
+
+        public void setUituCode(String uituCode) {
+            this.uituCode = uituCode;
         }
     }
 
--- src/main/java/ru/selsup/test/Main.java
+++ src/main/java/ru/selsup/test/Main.java
@@ -1,61 +1,46 @@
 package ru.selsup.test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.*;
 import java.net.InetSocketAddress;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) throws IOException {
         Logger logger = Logger.getLogger(Main.class.getName());
-        ObjectMapper objectMapper = new ObjectMapper();
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
         httpServer.createContext("/", new httpHandler());
         httpServer.setExecutor(null);
         httpServer.start();
         HttpClient httpClient = HttpClient.newHttpClient();
-        CrptApi crptApi = new CrptApi(httpClient, TimeUnit.SECONDS, 5, 5);
-        String documentContents = objectMapper.writeValueAsString(crptApi.getDocument());
-
-        HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(new URI("http://localhost:8080"))
-                .header("Content-Type", "application/json")
-                .GET()
-                .POST(HttpRequest.BodyPublishers.ofString(documentContents))
-                .build();
-        logger.info(httpRequest.toString());
+        CrptApi crptApi = new CrptApi(httpClient, TimeUnit.SECONDS, 3, 3);
+        CrptApi.Document document = crptApi.getDocument();
         Thread thread = new Thread(() -> {
             try {
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-                Thread.sleep(5500);
-                crptApi.processRequest(httpRequest);
-                crptApi.processRequest(httpRequest);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } catch (InterruptedException e) {
+                crptApi.createDocument(document, "1");
+                crptApi.createDocument(document, "2");
+                crptApi.createDocument(document, "3");
+                crptApi.createDocument(document, "1");
+                crptApi.createDocument(document, "2");
+                crptApi.createDocument(document, "3");
+                crptApi.createDocument(document, "1");
+                crptApi.createDocument(document, "2");
+                Thread.sleep(9500);
+                crptApi.createDocument(document, "3");
+                crptApi.createDocument(document, "1");
+                crptApi.createDocument(document, "2");
+                crptApi.createDocument(document, "3");
+                Thread.sleep(3500);
+                crptApi.createDocument(document, "1");
+                crptApi.createDocument(document, "2");
+            } catch (IOException | InterruptedException e) {
                 throw new RuntimeException(e);
             }
         });
